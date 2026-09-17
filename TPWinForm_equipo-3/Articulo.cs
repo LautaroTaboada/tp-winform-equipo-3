49902cfb5a18f7b177ff251a03146ccc517708b7
mateo-prog-23 <mateopisano23m@gmail.com>
2026-09-17 03:01:18 -0300

    Etapa 1 - Clases del modelo y conexion a base de datos

---
+++ TPWinForm_equipo-3/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_3
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; } // guarda la marca y su id
+        public Categoria Categoria { get; set; } // guarda la categoria y su id
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; } // Lista porque un articulo puede tener varias img
+    }
+}
---
+++ TPWinForm_equipo-3/ArticuloNegocio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TPWinForm_equipo_3
+{
+    internal class ArticuloNegocio
+    {
+        public List<Articulo> lista()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true;";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT Codigo, Nombre, Descripcion, Precio FROM ARTICULOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+            
+                lector = comando.ExecuteReader();
+
+                while (lector.Read()) 
+                {
+                    Articulo aux = new Articulo();
+                    aux.Codigo = (string)lector["Codigo"]; 
+                    aux.Nombre = (string) lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    aux.Precio = (Decimal)lector["Precio"];
+
+                    lista.Add(aux);
+                }
+                conexion.Close();
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
